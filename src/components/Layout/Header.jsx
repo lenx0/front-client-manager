6a99e8bb4e4e442456437f48877ca77b8833fc1c
thiago <thiagofreitas1106@gmail.com>
2024-06-29 16:05:58 -0300

    fix header layout

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -9,43 +9,35 @@ const Header = () => {
     return (
         <AppBar position="fixed" sx={{ bgcolor: 'primary.main' }}>
             <Toolbar>
-                <Grid container alignItems="center" m="0 3px 0 3px">
-                    <Grid item xs={isMobile ? 1 : 1}>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="open drawer"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                    </Grid>
-
-                    <Grid item xs={isMobile ? 8 : 9}>
-                        <Box display="flex" justifyContent="end" alignItems="center">
-                            <Box display="flex" alignItems="center">
-                                <SearchIcon style={{ marginRight: '10px', marginLeft: '20px' }} />
-                                <Box width={'100%'}>
-                                    <InputBase
-                                        placeholder="Search..."
-                                        inputProps={{ 'aria-label': 'search' }}
-                                        style={{ width: '100%', color: 'white', padding: '5px', border: '1px solid #d8d8d88d', borderRadius: '2px' }}
-                                    />
-                                </Box>
-                            </Box>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={isMobile ? 1 : 1} sx={{ mr: 3 }}>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="open drawer"
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Box display="flex" gap={3} width="100%" justifyContent="end" alignContent="center" alignItems="center">
+                    <Box backgroundColor="#bdbdbd" borderRadius="50%">
                         <IconButton color="inherit" >
-                            <Badge badgeContent={4} color="error" >
+                            <Badge
+                                badgeContent={100}
+                                color="error"
+                                sx={{
+                                    '& .MuiBadge-badge': {
+                                        minWidth: '16px',
+                                        height: '16px',
+                                        fontSize: '10px',
+                                        transform: 'translate(20px, -15px)'
+                                    }
+                                }}
+                            >
                                 <NotificationsIcon />
                             </Badge>
                         </IconButton>
-                    </Grid>
-                    <Grid item xs={isMobile ? 1 : 1}>
-                        <Avatar alt="User Avatar" src="/avatar.jpg" />
-                    </Grid>
-                </Grid>
+                    </Box>
+                    <Avatar alt="User Avatar" src="/avatar.jpg" />
+                </Box>
             </Toolbar>
         </AppBar>
     );
