d099636f71b1afb0814da77ddd3eed1dfd6fe597
thiago <thiagofreitas1106@gmail.com>
2024-07-03 21:45:29 -0300

    fix some things to deploy on vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { Grid } from '@mui/material'
-import './App.css'
-import UserForm from './components/UserForm'
-import UserList from './components/UserList'
-import Header from './components/layout/Header'
-import Sidebar from './components/layout/Sidebar'
-import Home from './components/pages/Home'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { ThemeProvider, CssBaseline } from '@mui/material'
-import lightTheme from './styles/theme/lightTheme'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { store, persistor } from './store'
+import React from 'react';
+import { Grid, ThemeProvider, CssBaseline } from '@mui/material';
+import './App.css';
+import UserForm from './components/UserForm';
+import UserList from './components/UserList';
+import Header from './components/layout/Header';
+import Sidebar from './components/layout/Sidebar';
+import Home from './components/pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import lightTheme from './components/styles/theme/lightTheme';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
 
 function App() {
-  console.log(lightTheme)
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider theme={lightTheme}>
           <CssBaseline />
-          <BrowserRouter >
+          <BrowserRouter>
             <Grid container>
               <Grid item xs={12}>
                 <Header />
               </Grid>
-              {/* <Grid item xs={2}> */}
               <Grid item>
                 <Sidebar />
               </Grid>
-              {/* <Grid item xs={10}> */}
               <Grid item>
                 <Grid margin="120px 50px 50px 50px">
                   <Routes>
@@ -43,7 +40,7 @@ function App() {
         </ThemeProvider>
       </PersistGate>
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
     },
   },
-})
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-redux'],
+    },
+  },
+});
