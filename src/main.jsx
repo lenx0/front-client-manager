76509c03b685bdefbaee870dc2b86dfe6f59dd97
thiago <thiagofreitas1106@gmail.com>
2024-07-03 21:12:06 -0300

    fix import on main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from '@emotion/react'
-import theme from './components/styles/theme.lightTheme.js'
+import theme from './components/styles/theme/lightTheme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
