210ebcaab21138f9bac70dd1c084fe598228b551
thiago <thiagofreitas1106@gmail.com>
2024-07-03 21:41:59 -0300

    remove imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ThemeProvider } from '@emotion/react'
-import lightTheme from '@components/styles/theme';
+// import { ThemeProvider } from '@emotion/react'
+// import lightTheme from '@components/styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
