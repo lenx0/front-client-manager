50ce979d6cf08c8c159314c852ec9459ff8351b6
thiago <thiagofreitas1106@gmail.com>
2024-07-03 21:20:40 -0300

    fix vercel problem on theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from '@emotion/react'
-import theme from './components/styles/theme/lightTheme'
+import lightTheme from './components/styles/theme/lightTheme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={lightTheme}>
     <App />
     </ThemeProvider>
   </React.StrictMode>,
